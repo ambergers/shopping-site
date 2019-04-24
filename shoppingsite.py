79ebe11a81e12bbcd9838fec76682fd8198daeb4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-24 12:59:18 -0700

    Added add to cart logic and fixed melon details page

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -48,7 +48,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -97,8 +97,13 @@ def add_to_cart(melon_id):
     # - increment the count for that melon id by 1
     # - flash a success message
     # - redirect the user to the cart page
+    cart = session.get("cart", {})
+    cart[melon_id] = cart.get(melon_id, 0) + 1
 
-    return "Oops! This needs to be implemented!"
+    flash("Melon was successfully added to your cart")
+    print(cart)
+
+    return redirect('/cart')
 
 
 @app.route("/login", methods=["GET"])
